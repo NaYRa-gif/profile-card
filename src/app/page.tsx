38c4dc588a17983bc662a57aa940daa0a09bc873
NaYRa-gif <romuandedamame0606@gmail.com>
2026-02-03 16:04:01 +0900

    アバター画像の修正（未解決　）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const dummyProfile: UserProfiles = {
     name: "John Doe",
     age: 28,
     location: "Tokyo, Japan",
-    avataUrl: "@/public/avatar.JPG",
+    avataUrl: "/Users/mt.circle/profile-card/public/avatar.png",
     stats: {
         followers: "1.2K",
         likes: "3.4K",
